fc3614693ff1dc1f144e9275d3d04fadb4b493ff
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-03-14 15:09:40 -0300

    add recipe details

--- lib/screens/recipe_details_screen.dart
+++ lib/screens/recipe_details_screen.dart
@@ -3,6 +3,30 @@ import 'package:flutter/material.dart';
 import '../models/recipe.dart';
 
 class RecipeDetailsScreen extends StatelessWidget {
+  Widget _createSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.bodyText1,
+      ),
+    );
+  }
+
+  Widget _createSectionContainer(Widget child) {
+    return Container(
+      width: 300,
+      height: 200,
+      padding: const EdgeInsets.all(12),
+      margin: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(12)),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final Recipe recipe = ModalRoute.of(context).settings.arguments;
@@ -11,8 +35,56 @@ class RecipeDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(recipe.title),
       ),
-      body: Center(
-        child: Text('Recipe Details'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              child: Image.network(
+                recipe.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            _createSectionTitle(context, 'Ingredients'),
+            _createSectionContainer(
+              ListView.builder(
+                itemCount: recipe.ingredients.length,
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 6,
+                        horizontal: 10,
+                      ),
+                      child: Text(recipe.ingredients[index]),
+                    ),
+                    color: Theme.of(context).accentColor,
+                  );
+                },
+              ),
+            ),
+            _createSectionTitle(context, 'Steps'),
+            _createSectionContainer(
+              ListView.builder(
+                itemCount: recipe.steps.length,
+                itemBuilder: (ctx, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('${index + 1}'),
+                        ),
+                        title: Text(recipe.steps[index]),
+                      ),
+                      if (index < recipe.steps.length - 1) Divider(),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
