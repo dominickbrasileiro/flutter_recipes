b4cfad8c697c75c1656f352504927f16d7345ec0
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-03-14 12:33:22 -0300

    add navigation to category recipes screen on category item tap

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,30 +1,44 @@
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
+import '../screens/category_recipes_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem({Key key, this.category}) : super(key: key);
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) {
+        return CategoryRecipesScreen();
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).accentColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.bodyText1,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
       ),
     );
   }
